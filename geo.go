4b3688dde57131a3339217eb7c270a001c98274c
Mr.Shark <mist>
2016-09-22 19:02:02 +0800

    add geo

--- geo.go
+++ geo.go
@@ -48,6 +48,42 @@ func (geo *Geo) Find_ip(ip string) (isp, province string, err error) {
 	return
 }
 
+type GeoIPEntry struct {
+	Country  string `json:"country,omitempty"`
+	Province string `json:"Province,omitempty"`
+	City     string `json:"City,omitempty"`
+	Village  string `json:"Village,omitempty"`
+	Isp      string `json:"Isp,omitempty"`
+}
+
+func (geo *Geo) FindGeo_ip(ip string) (geoipentry GeoIPEntry, err error) {
+	var null *C.struct_out_entry
+	p := C.find_ip(geo.ips, C.CString(ip))
+	if p != null {
+		isp := C.GoString(p.isp)
+		if len(isp) > 2 && isp[:2] == "i_" {
+			isp = isp[2:]
+		}
+		province := C.GoString(p.province)
+		if len(province) > 2 && province[:2] == "p_" {
+			province = province[2:]
+		}
+		city := C.GoString(p.city)
+		village := C.GoString(p.village)
+		country := C.GoString(p.country)
+
+		geoipentry.City = city
+		geoipentry.Country = country
+		geoipentry.Province = province
+		geoipentry.Village = village
+		geoipentry.Isp = isp
+
+	} else {
+		err = errors.New("not found")
+	}
+	return
+}
+
 func open_ips(filename string, flags uint32) (ips *C.struct_ips_t, err error) {
 	var null *C.struct_ips_t
 
--- geo_test.go
+++ geo_test.go
@@ -6,20 +6,40 @@ import (
 )
 
 func TestAll(t *testing.T) {
+
+	fmt.Println("--------find begin--------")
 	{
-		geo, _ := New("./ip_sample.txt", 0)
+		geo, _ := New("./ip_sample.txt", GEO_N_ALIAS)
 		fmt.Println(geo.Find_ip("1.0.167.5"))
 		fmt.Println(geo.Find_ip("1.0.165.5"))
 		fmt.Println(geo.Find_ip("1.0.8.5"))
 		geo.Clean()
 	}
 
-	fmt.Println("--------alias begin--------")
+	fmt.Println("--------find alias begin--------")
 	{
 		geo, _ := New("./ip_sample.txt", GEO_F_ALIAS)
 		fmt.Println(geo.Find_ip("1.0.167.5"))
 		fmt.Println(geo.Find_ip("1.0.165.5"))
 		fmt.Println(geo.Find_ip("1.0.8.5"))
+		geo.Clean()
+	}
+
+	fmt.Println("--------geo begin--------")
+	{
+		geo, _ := New("./ip_sample.txt", GEO_F_ALIAS)
+		fmt.Println(geo.FindGeo_ip("1.0.167.5"))
+		fmt.Println(geo.FindGeo_ip("1.0.165.5"))
+		fmt.Println(geo.FindGeo_ip("1.0.8.5"))
+		geo.Clean()
+	}
+
+	fmt.Println("--------geo alias begin--------")
+	{
+		geo, _ := New("./ip_sample.txt", GEO_F_ALIAS)
+		fmt.Println(geo.FindGeo_ip("1.0.167.5"))
+		fmt.Println(geo.FindGeo_ip("1.0.165.5"))
+		fmt.Println(geo.FindGeo_ip("1.0.8.5"))
 		geo.Clean()
 	}
 }
